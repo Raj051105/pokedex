9459f22bd1bf9a3ab7ab56edb687fde2c408d545
Sivakanesh <sksivakanesh12@gmail.com>
2023-12-04 21:57:25 +0530

    templates folder added

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask ,render_template
+#from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello world!"
+def index():
+    return render_template("test.html")
 if __name__ == "__main__":
       app.run(host="0.0.0.0",port=5000)
