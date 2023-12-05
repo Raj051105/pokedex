fdfe9a0ac3ae84198a50dad1677fa18468d2371d
Sivakanesh <sksivakanesh12@gmail.com>
2023-12-05 22:11:26 +0530

    mithun work merged

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask ,render_template
-
+#from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route("/")
+
 def index():
     return render_template("test.html")
 if __name__ == "__main__":
