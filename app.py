177476b66dcc05522a8fe39e158e8879ca62afae
Mitun <mitun.7557@gmail.com>
2023-12-31 12:50:16 +0530

    Enhanced UI

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask ,render_template
-#from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,4 +10,3 @@ def index():
     return render_template("test.html")
 if __name__ == "__main__":
       app.run(host="0.0.0.0",port=5000)
-      
